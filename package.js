2d5e7d35e3699c809540bba1a82ad7ec6209b119
Tim Brandin <tim.brandin@gmail.com>
2015-06-30 09:19:08 +0200

    Updates to readme.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'timbrandin:jsx-templating',
-  version: '0.1.1',
+  version: '0.1.2',
   // Brief, one-line summary of the package.
   summary: 'React jsx templating',
   // URL to the Git repository containing the source code for this package.
