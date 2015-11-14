45a77e43c245a367896a4133d64e9e0ba36c1afd
Tim Brandin <tim.brandin@gmail.com>
2015-11-14 11:41:37 +0100

    Removing redundant comments.

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'timbrandin:sideburns',
   version: '0.3.2',
-  // Brief, one-line summary of the package.
   summary: 'React templates for Meteor',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/timbrandin/meteor-react-sideburns',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
